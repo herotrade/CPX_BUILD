948f1e42e6998daa6c5170db390a4d2705b91f1d
mac-m4-2 <mac-m4-2@algoquant.com>
2025-11-10 17:03:11 +0800

    fix

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ NGINX_TEMPLATE="nginx/conf.d/cpx_exchange.temp.conf"
 NGINX_CONFIG="nginx/conf.d/cpx_exchange.conf"
 
 APP_REPO_URL=${APP_REPO_URL:-https://github.com/herotrade/CPX_EXCHANGE.git}
-APP_REPO_BRANCH=${APP_REPO_BRANCH:-prod}
+APP_REPO_BRANCH=${APP_REPO_BRANCH:-release}
 APP_REPO_NAME=${APP_REPO_NAME:-$(basename -s .git "${APP_REPO_URL}")}
 APP_PARENT_DIR=${APP_PARENT_DIR:-"${SCRIPT_DIR}/app"}
 APP_CLONE_DIR=${APP_CLONE_DIR:-"${APP_PARENT_DIR}/${APP_REPO_NAME}"}
